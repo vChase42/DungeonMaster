6169d774a4f98b719c42fd6d2fdc3a78597115a0
sergey <sergeyalkorn@gmail.com>
2024-09-04 15:41:48 -0700

    made single question ai funciton, modified the stream function,

--- ai_interaction.py
+++ ai_interaction.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 
 class aiConnection:
-    
+
     def __init__(self) -> None:
         self.url = os.getenv('CHAT_AI_URL')
         self.model = os.getenv('MODEL')
@@ -14,6 +14,7 @@ class aiConnection:
         self.blankHistory()
 
     def testAiConnection(self):
+
         response = self.client.chat(model='gemma_abl_27b', messages=[
         {
             'role': 'user',
@@ -26,33 +27,44 @@ class aiConnection:
             print("Something is wrong")
             
     def showParams(self):
+
         print(f"The URL is: {self.url}")
         print(f"The model is currently: {self.model}")
         print(f"The client: {self.client}")
         print(f"The system prompt is as follows: {self.sysPrompt}")
         print(f"History: {self.history}")
     
-        
     def appendHistory(self,role,message):
-            self.history.append({
-            'role': role,
-            'content': message
+        """appendHistory appends the message and who sent it to the running history of the chat
+
+        Arguments:
+            role -- role is either user, assistant, or system.
+            message -- message to be appended to the current history
+        """
+        self.history.append({
+        'role': role,
+        'content': message
         })
 
     def askAi(self,newMessage):
         self.appendHistory(role = 'user',message = newMessage)
         aiResponse = self.client.chat(model=self.model, messages=self.history)
         self.appendHistory(role = 'assistant',message = aiResponse)
+        return aiResponse['message']['content']
         
-    def getAiResponseAsync(self, newMessage):
+    def askAiOnce(self, newMessage):
+        aiResponse = self.client.chat(model = self.model, messages = [{'role':'user','content':newMessage}])
+        return(aiResponse['message']['content'])
+        
+    def getAiResponseAsync(self, newMessage): 
         self.appendHistory(role = 'user',message = newMessage)
         aiResponse = ""
         for chunk in self.client.chat(model=self.model, messages=self.history, stream=True):
             aiResponse += chunk.get("message").get('content')
-            yield f"{chunk.get("message").get('content')}"  # Proper SSE format
+            yield f"{chunk.get('message').get('content')}"  # Proper SSE format
         self.appendHistory(role = 'assistant',message = aiResponse)
         
-    def blankHistory(self):
+    def blankHistory(self): #removes all the history of the chat except the system prompt
         self.history = [{
         'role': 'system',
         'content': self.sysPrompt,
@@ -61,37 +73,12 @@ class aiConnection:
 
 if __name__ == '__main__':
     ai = aiConnection()
-    #ai.testAiConnection()
+    ai.testAiConnection()
     ai.showParams()
-    x= ai.askAi("tell me a joke")
-    print(x)
-    ai.showParams()
-    y = ai.askAi("another one")
-    print(y)
-    ai.showParams()
+    print('-------------------')
+    response = ai.askAi('hello')
+    print(response)
+    print('-------------------')
+    response = ai.askAiOnce('hi')
+    print(response)
     
-    
-   # ai.showParams()
-    
-        
-
-
-
-
-# def ai_thing(user_input):
-    # global CHAT_HISTORY
-    # CHAT_HISTORY.append({
-    #     'role':'user',
-    #     'content': user_input
-    # })
-##
-    # ai_response = ""
-    # # Stream the API response
-    # for chunk in client.chat(model='gemma_abl_27b', messages=CHAT_HISTORY, stream=True):
-    #     ai_response += chunk.get("message").get('content')
-    #     yield f"{chunk.get("message").get('content')}"  # Proper SSE format
-    #     # print(ai_response)
-    # CHAT_HISTORY.append({
-    #     'role':'assistant',
-    #     'content':ai_response
-    #     })
